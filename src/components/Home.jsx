57c2b87d5195cda6d5d084e27a4918d4cad8ce06
DuckylikesBatman <maher.aboudiab@hotmail.com>
2026-04-06 10:47:33 +0300

    initial

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import "../styles/Home.css"
+import "../styles/app.css"
+function Home() {
+    return (
+        <div className="home-page">
+    <h1>Welcome to the Apps</h1>
+    </div>
+    )
+  }
+  
+  export default Home;
+  
---
+++ src/components/Login.jsx
@@ -0,0 +1,26 @@
+  import "../styles/login.css";
+  import bg from "../assets/images/login-bg.jpeg";
+  import { useNavigate } from "react-router-dom";
+
+  
+
+  function Login() {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+      // later: validate with backend
+      navigate("/home");
+    };
+    return (
+      <div className="login-page" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="login-container">
+          <input className="login-inputs" placeholder="Username" />
+          <input className="login-inputs" placeholder="Password" />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      </div>
+    );
+    
+  }
+
+  export default Login;
