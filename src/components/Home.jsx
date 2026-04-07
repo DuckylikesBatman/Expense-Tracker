b8c08da98efed93c3177093d4780ee94299d6632
DuckylikesBatman <maher.aboudiab@hotmail.com>
2026-04-07 19:34:43 +0300

    fixed buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,25 @@ function Home() {
         <div className="home-page">
             <button className="back-arrow" onClick={() => navigate("/")}>&#8592;</button>
             <nav className="Nav-bar">
-                <button className="nav-buttons">Home</button>
-                <button className="nav-buttons">Expenses</button>
-                <button className="nav-buttons">About</button>
+                <button className="buttons">Home</button>
+                <button className="buttons">Expenses</button>
+                <button className="buttons">About</button>
             </nav>
+            <div className="Total-Expenses">
+                <h1>Total Spent</h1>
+
+            </div>
             <div className="form-expense">
                 <h1 className="Titles">Expenses</h1>
                 <input type="text" className="input-expenses" placeholder="Add Expense"></input>
                 <input type="number" className="input-expenses" placeholder="Add Amount"></input>
+                <div className="category-grid">
+                    <button className="buttons expense">Food</button>
+                    <button className="buttons expense">Travel</button>
+                    <button className="buttons expense">Utilities</button>
+                    <button className="buttons expense">Shopping</button>
+                    <button className="buttons expense">Other</button>
+                </div>
             </div>
     </div>
     )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,9 +15,12 @@
     return (
         <div className="login-card">
           <h1 className="login-header">Expense Tracker</h1>
+          <h3>Welcome Back</h3>
+          <h6>Please enter your credentials to log in</h6>
           <input className="login-inputs" placeholder="Username" />
           <input className="login-inputs" placeholder="Password" />
-          <button onClick={handleLogin} className="button">Login</button>
+          <button onClick={handleLogin} className="buttons">Login</button>
+          <h4> Don't have an account?<a className="sign-up" href="sign-up.html"> Sign Up</a> </h4>
         </div>
     );
     
