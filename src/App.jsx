059be7e1c366d4747107f586479b4cd9036299bd
DuckylikesBatman <maher.aboudiab@hotmail.com>
2026-04-11 23:57:13 +0300

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import signup from "./components/SignuP";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<signup />} />
       </Routes>
     </Router>
   );
---
+++ src/components/SignuP.jsx
@@ -0,0 +1,9 @@
+import "../styles/app.css"
+import "../styles/signup.css"
+
+
+function Signup(){
+    <div>
+        <h1> This is the SignUp Page</h1>
+    </div>
+}
